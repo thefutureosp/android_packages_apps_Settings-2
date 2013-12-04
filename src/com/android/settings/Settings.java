2f90802dc53f0121bf86be79d52901df05e387b0
cristianomatos <cristianobmatos@gmail.com>
2013-12-04 09:57:57 -0200

    RAM bar (2/2)
    Thanks to @kufikugel for hole code ported from 4.3
    
    Change-Id: Icc2307901a9052c647e52c3d3cb6f00805e88aba
    
    Conflicts:
    	res/values/cr_strings.xml

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1158,4 +1158,5 @@ public class Settings extends PreferenceActivity
     public static class PrintJobSettingsActivity extends Settings { /* empty */ }
     public static class AnonymousStatsActivity extends Settings { /* empty */ }
     public static class BlacklistSettingsActivity extends Settings { /* empty */ }
+    public static class ASSRamBarActivity extends Settings { /* empty */ } 
 }
