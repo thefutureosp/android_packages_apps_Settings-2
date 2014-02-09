42c0abc72746eb6bba7c2c648967d94f6a7d57e7
thefutureosp <drewg29@lycos.com>
2014-02-09 02:51:15 -0500

    Fix linkage

--- src/com/android/settings/orca/NotificationDrawerStyle.java
+++ src/com/android/settings/orca/NotificationDrawerStyle.java
@@ -37,7 +37,7 @@ import android.util.Log;
 import android.view.Display;
 import android.widget.Toast;
 
-import com.android.internal.util.crdroid.DeviceUtils;
+import com.android.internal.util.orca.DeviceUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
